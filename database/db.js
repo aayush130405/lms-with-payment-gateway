aba4f5987497838feee383e0e2ff07b4267b3ab7
Aayush Yadav <aayush130405@gmail.com>
2025-10-28 00:04:59 +0530

    add strictQuery method

--- database/db.js
+++ database/db.js
@@ -7,5 +7,8 @@ class DatabaseConnection {
     constructor() {
         this.retryCount = 0;
         this.isConnected = false;
+
+        //configure mongoose settings
+        mongoose.set('strictQuery', true)   //strictQuery is useful when extracting data from db, and basically only search for the fields that are in the schema and remove those that are extra
     }
 }
