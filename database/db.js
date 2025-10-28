95fe98ebaff6892d8676a9851e9e3426e84cbd92
Aayush Yadav <aayush130405@gmail.com>
2025-10-29 00:00:59 +0530

    configure connection state in mdb

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const MAX_RETRIES = 3
 const RETRY_INTERVAL = 5000 //5 seconds
@@ -24,5 +24,27 @@ class DatabaseConnection {
             this.isConnected = false;
             //TODO: attempt a reconnection
         })
+    }
+
+    async connect() {
+        if(!process.env.MONGO_URI) {
+            throw new Error("MONGODB URI is not defined in .env file")
+        }
+
+        const connectionOptions = {
+            useNewUrlParser: true,  //Tells Mongoose to use the new way of parsing the MongoDB connection string (URL)
+            useUnifiedTopology: true,   //Enables the new connection engine (unified topology) that MongoDB introduced
+            maxPoolSize: 10,    //Sets the maximum number of connections that can be kept open in the pool
+            serverSelectionTimeoutMS: 5000,     //The maximum time (in milliseconds) your app will wait while trying to find a working MongoDB server
+            socketTimeoutMS: 45000,     //The maximum time (in milliseconds) a socket (network connection) stays open when waiting for a response
+            family: 4       //Whether to connect using IPv4 or IPv6, 4 in this case
+        }
+
+        if(process.env.NODE_ENV === 'development') {
+            mongoose.set('debug', true)
+        }
+
+        await mongoose.connect(process.env.MONGO_URI, connectionOptions)
+        this.retryCount = 0;        //set retryCount as 0 bcz connection is successful
     }
 }
