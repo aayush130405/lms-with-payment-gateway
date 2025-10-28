f3323c6af398f024793a206e460bb38057d481fa
Aayush Yadav <aayush130405@gmail.com>
2025-10-28 23:37:20 +0530

    configure .on connection for mongodb

--- database/db.js
+++ database/db.js
@@ -10,5 +10,19 @@ class DatabaseConnection {
 
         //configure mongoose settings
         mongoose.set('strictQuery', true)   //strictQuery is useful when extracting data from db, and basically only search for the fields that are in the schema and remove those that are extra
+
+        mongoose.connection.on('connected', () => {
+            console.log("MONGODB is CONNECTED");
+            this.isConnected = true;
+        })
+        mongoose.connection.on('error', () => {
+            console.log("MONGODB connection ERROR");
+            this.isConnected = false;
+        })
+        mongoose.connection.on('disconnected', () => {
+            console.log("MONGODB is DISCONNECTED");
+            this.isConnected = false;
+            //TODO: attempt a reconnection
+        })
     }
 }
