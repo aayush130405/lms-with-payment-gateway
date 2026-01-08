59e83e8a978e9183984be9042443d2cb66f2882a
Aayush Yadav <aayush130405@gmail.com>
2026-01-08 22:02:22 +0530

    make controllers and routes directory

---
+++ controllers/health.controller.js
@@ -0,0 +1,11 @@
+function getReadyStateText(state) {
+    switch (state) {
+        case 0: return 'Disconnected';
+        case 1: return 'Connected';
+        case 2: return 'Connecting';
+        case 3: return 'Disconnecting';
+            
+        default: return 'Unknown';
+    }
+
+}
