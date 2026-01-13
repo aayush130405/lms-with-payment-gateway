59e4916153f0945e8961c5c74f1a64e36a20ae7b
Aayush Yadav <aayush130405@gmail.com>
2026-01-13 12:35:12 +0530

    configure health controller

--- controllers/health.controller.js
+++ controllers/health.controller.js
@@ -1,3 +1,41 @@
+import { getDBStatus } from "../database/db.js";
+
+export const checkHealth = async (req, res) => {
+    try {
+        const dbStatus = getDBStatus(); 
+    
+        const healthStatus = {
+            status: 'OK',
+            timestamp: new Date().toISOString(),
+            services: {
+                database: {
+                    status: dbStatus.isConnected ? 'Healthy' : 'Unhealthy',
+                    details: {
+                        ...dbStatus,
+                        readyState: getReadyStateText(dbStatus.readyState)
+                    }
+                },
+                server: {
+                    status: 'Healthy',
+                    uptime: process.uptime(),
+                    memoryUsage: process.memoryUsage
+                }
+            }
+        }
+    
+        const httpStatus = healthStatus.services.database.status === 'Healthy' ? 200 : 503;
+    
+        res.status(httpStatus).json(healthStatus);
+    } catch (error) {
+        console.error('Health check failed', error);
+        res.status(500).json({
+            status: "ERROR",
+            timestamp: new Date().toISOString(),
+            error: error.message
+        })
+    }
+}
+
 //function to get readyState text
 function getReadyStateText(state) {
     switch (state) {
