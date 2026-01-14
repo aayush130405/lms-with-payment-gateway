283d59493c381999a91d5d7f40f447ffe3af7a11
Aayush Yadav <aayush130405@gmail.com>
2026-01-14 14:05:17 +0530

    add global async handler and standardise error response

---
+++ middleware/error.middleware.js
@@ -0,0 +1,15 @@
+export class ApiError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'Fail' : 'Error';
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export const catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next);
+    }
+}
