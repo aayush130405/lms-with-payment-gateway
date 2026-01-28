fcade73f2ac597d0e619c825cac7b694baa19564
Aayush Yadav <aayush130405@gmail.com>
2026-01-28 12:42:53 +0530

    add user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import {createUserAccount} from '../controllers/user.controller.js';
+import {authenticateUser, createUserAccount, getCurrentUserProfile, signOutUser, updateUserProfile} from '../controllers/user.controller.js';
+import { isAuthenticated } from "../middleware/auth.middleware.js";
+import upload from '../utils/multer.js';
 
 const router = express.Router();
 
 router.post('/signup', createUserAccount);
+router.post('/signin', authenticateUser);
+router.post('/signout', signOutUser);
 
-
+router.get('/profile', isAuthenticated, getCurrentUserProfile);
+router.patch('/profile', isAuthenticated, upload.single('avatar'), updateUserProfile);
 
 export default router;
