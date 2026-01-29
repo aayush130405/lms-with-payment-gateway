965e6eacd7c65e681ae5438c4001428f34fc1f2c
Aayush Yadav <aayush130405@gmail.com>
2026-01-29 23:31:11 +0530

    add validateSignup middleware in /signup route

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,10 +2,11 @@ import express from "express";
 import {authenticateUser, createUserAccount, getCurrentUserProfile, signOutUser, updateUserProfile} from '../controllers/user.controller.js';
 import { isAuthenticated } from "../middleware/auth.middleware.js";
 import upload from '../utils/multer.js';
+import { validateSignup } from "../middleware/validation.middleware.js";
 
 const router = express.Router();
 
-router.post('/signup', createUserAccount);
+router.post('/signup', validateSignup, createUserAccount);
 router.post('/signin', authenticateUser);
 router.post('/signout', signOutUser);
 
